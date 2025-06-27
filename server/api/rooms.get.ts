08953153f6280b4d70d5dc7fb140cda7aee9da81
Tksi <24800668+Tksi@users.noreply.github.com>
2025-06-28 02:38:31 +0900

    feat: ルーム自動削除

--- server/api/rooms.get.ts
+++ server/api/rooms.get.ts
@@ -3,6 +3,15 @@ import type { Room } from '~/types';
 import { rooms } from '~/server/state/rooms';
 
 export default defineEventHandler(() => {
+  // 1日以上経ったルームを削除
+  const now = Date.now();
+
+  for (const [roomId, room] of rooms.entries()) {
+    if (now - room.createdAt > 24 * 60 * 60 * 1000) {
+      rooms.delete(roomId);
+    }
+  }
+
   return rooms
     .values()
     .toArray()
