02e3c30bc50a1e469174019069636c1100a61213
Tksi <24800668+Tksi@users.noreply.github.com>
2025-06-28 18:51:57 +0900

    chore: format

--- server/lib/replacer.ts
+++ server/lib/replacer.ts
@@ -1,8 +1,8 @@
 /**
  * Map対応JSON.stringifyのreplacer関数
- * @param _　キー
- * @param value　値
- * @returns　値をMapからオブジェクトに変換したもの
+ * @param _ キー
+ * @param value 値
+ * @returns 値をMapからオブジェクトに変換したもの
  */
 export const replacer = (_: unknown, value: unknown): unknown => {
   if (value instanceof Map) return Object.fromEntries(value);
